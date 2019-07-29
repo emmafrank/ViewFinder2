2431d9114ee8b5229293c5e375bceb5ffcc55ea7
Apple <apple@Apples-MacBook-Pro-7.local>
2019-07-29 15:27:03 -0400

    message

---
+++ ViewFinder/AddPhotoViewController.swift
@@ -0,0 +1,51 @@
+//
+//  AddPhotoViewController.swift
+//  ViewFinder
+//
+//  Created by Apple on 7/29/19.
+//  Copyright © 2019 KWK. All rights reserved.
+//
+
+import UIKit
+
+class AddPhotoViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    var imagePicker = UIImagePickerController()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        imagePicker.delegate = self
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func cameraClicked(_ sender: Any) {
+        imagePicker.sourceType = .camera
+        present(imagePicker, animated: true, completion: nil)
+    }
+   
+    
+    @IBAction func libraryClicked(_ sender: Any) {
+        imagePicker.sourceType = .photoLibrary
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    @IBOutlet weak var newImage: UIImageView!
+    private func imagePickerController( picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let selectedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            newImage.image = selectedImage
+        }
+        // go back to our ViewController so the user can see the update
+        // updates photo w selected photo
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
